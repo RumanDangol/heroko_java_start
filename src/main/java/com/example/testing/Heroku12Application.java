d956e168889eebdad2903121bc513fe5e76a5888
ruman-fusemachines <ruman@fusemachines.com>
2018-05-14 14:47:43 +0545

    project starting

---
+++ src/main/java/com/example/testing/Heroku12Application.java
@@ -0,0 +1,12 @@
+package com.example.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Heroku12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Heroku12Application.class, args);
+	}
+}
